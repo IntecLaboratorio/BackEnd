522d6f51f7ff3d284928c45471cee8db684171a1
Gustavo Augusto Wustemberg <gustavowustemberg14@gmail.com>
2022-10-06 22:28:17 -0300

    alt user api

--- src/services/userService.js
+++ src/services/userService.js
@@ -3,9 +3,9 @@ import database from "../repository/connection.js";
 async function insertUser(id_corporate, address, type_user, name_user, cpf, rg, phone, email, password, verify) {
   const conn = await database.connect();
 
-  const sql = 'INSERT INTO users_tbl(id_corporate, address, type_user, name_user, cpf, rg, phone, email, password, verify) VALUES(?,?,?,?,?,?,?,?,?,?);';
-  const dataUser = [id_corporate, address, name_user, last_name, cpf, rg, phone, email, password, verify];
-  // console.log(dataUser);
+  const sql = 'INSERT INTO users_tbl(fk_id_corporate, fk_address, fk_typeUser, name_user, cpf, rg, phone, email, senha, verify) VALUES(?,?,?,?,?,?,?,?,?,?);';
+  const dataUser = [id_corporate, address, type_user, name_user, cpf, rg, phone, email, password, verify];
+  console.log(dataUser);
 
   await conn.query(sql, dataUser);
 
@@ -22,7 +22,7 @@ async function findUser() {
 }
 async function upadateUser(id_corporate, address, type_user, name_user, cpf, rg, phone, email, password, verify, id) {
   const conn = await database.connect();
-  const sql = 'UPDATE users_tbl SET fk_id_corporate = ?, fk_address = ?, name_user = ?, last_name = ?, cpf = ?, rg = ?, phone = ?, email = ?, senha = ?, verify = ? WHERE id = ?';
+  const sql = 'UPDATE users_tbl SET fk_id_corporate = ?, fk_address = ?, fk_typeUser = ?, name_user = ?, cpf = ?, rg = ?, phone = ?, email = ?, senha = ?, verify = ? WHERE id = ?';
   const dataUser = [id_corporate, address, type_user, name_user, cpf, rg, phone, email, password, verify, id];
   await conn.query(sql, dataUser);
   conn.end();
