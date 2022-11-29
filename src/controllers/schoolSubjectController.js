027f2011cd696e5e20b0702ccf22ebf69d36e695
Gustavo Augusto Wustemberg <gustavowustemberg14@gmail.com>
2022-11-28 22:06:16 -0300

    alt school

--- src/controllers/schoolSubjectController.js
+++ src/controllers/schoolSubjectController.js
@@ -4,10 +4,7 @@ import db from '../services/schoolSubjectService.js';
 
 const router = Express.Router();
 
-router.post('/', [
-    body('name_school_subject').isString().withMessage("Por favor, insira a disciplina corretamente."),
-    body('abbreviation').isString().withMessage('Insira a sigla do seu curso. Ex: ADM, DS, REDES'),
-], async (request, response) => {
+router.post('/', async (request, response) => {
 
     const errors = validationResult(request)
     if (!errors.isEmpty()) {
@@ -15,10 +12,10 @@ router.post('/', [
         return response.status(400).json({ message: errors.array() });
     }
 
-    const { name_school_subject, abbreviation, schoolModule } = request.body
+    const { name_school_subjetc, abbreviation, schoolModule } = request.body
 
     try {
-        await db.insertSchoolSubject(name_school_subject, abbreviation, schoolModule);
+        await db.insertSchoolSubject(name_school_subjetc, abbreviation, schoolModule);
         response.status(201).json({ message: 'Disciplina cadastrada com sucesso' })
 
     } catch (error) {
