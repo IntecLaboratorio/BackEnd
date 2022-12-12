dea059d8cf1e0d2abbcbca2638e767719541ab1f
Guilherme Silva <guilhermesribeiro05@gmail.com>
2022-12-12 19:01:45 -0300

    alteracoes

--- src/repository/connection.js
+++ src/repository/connection.js
@@ -6,10 +6,19 @@ const connect = async () => { //conexão direta com banco
         user: "gsR_Silva",
         password: "gsr_SilvaR*07**",
         database: "tcc"
+
+        // MYSQL_ADDON_HOST: "b7v2h4kzpt3iw2vjllwf-mysql.services.clever-cloud.com",
+        // MYSQL_ADDON_DB: "b7v2h4kzpt3iw2vjllwf",
+        // MYSQL_ADDON_USER: "uxoik2dvwac185jw",
+        // MYSQL_ADDON_PORT: "3306",
+        // MYSQL_ADDON_PASSWORD: "wA0IfslY2P2Lz2rehMHk",
+        // MYSQL_ADDON_URI: "mysql://uxoik2dvwac185jw:wA0IfslY2P2Lz2rehMHk@b7v2h4kzpt3iw2vjllwf-mysql.services.clever-cloud.com:3306/b7v2h4kzpt3iw2vjllwf"
     });
 
     return connection
 
 }
 
-export default { connect };
+export default {
+    connect
+};
